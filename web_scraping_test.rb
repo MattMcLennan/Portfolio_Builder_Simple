73c6ae8f6b9242cc61f0f26f39c9f27c4afc4942
MattMcLennan <mclennan6891@gmail.com>
2015-10-31 19:47:15 +0000

    Web scraping implemented

---
+++ web_scraping_test.rb
@@ -0,0 +1,42 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+url = "http://ca.advfn.com/stock-market/TSX/BMO/financials"
+doc = Nokogiri::HTML(open(url))
+
+# The following code will get us all of the company specific data we need
+doc.css("table td").each do |i|
+  
+  if i.text == "Company Name: "
+    puts i.next.text
+  elsif i.text == "Industry Information: "
+    puts i.next.text.match(/[A-Z]+/)
+  elsif i.text == "PE Ratio - LTM"
+    puts i.next.text
+  elsif i.text == "Market Capitalisation"
+    if i.next.next.text == "mil"
+      val = i.next.text
+      puts  val.gsub(/[^\d\.]/, '').to_f * 1_000_000
+    end
+  elsif i.text == "Enterprise Value (EV)/EBITDA"
+    puts i.next.text
+  elsif i.text == "Enterprise Value (EV)/Free Cash Flow"
+    puts i.next.text
+  elsif i.text == "Price/Book Ratio - LTM"
+    puts i.next.text
+  elsif i.text == "Price/Free Cash Flow Ratio - LTM"
+    puts i.next.text
+  elsif i.text == "Return on Equity (ROE)"
+    puts i.next.text
+  elsif i.text == "Return on Capital Invested (ROCI)"
+    puts i.next.text
+  elsif i.text == "Return on Assets (ROA)"
+    puts i.next.text
+  elsif i.text == "EBITDA Margin - LTM"
+    puts i.next.text
+  elsif i.text == "Net Profit Margin"
+    puts i.next.text
+  end
+end
+
